2ffcb24ddd6d3e481467e13dc64c88b3bef26033
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-01 16:13:26 -0400

    adds static markup for table view -- partial

---
+++ frontend/src/Table.jsx
@@ -0,0 +1,14 @@
+import './Table.css';
+
+function Table() {
+  return (
+    <main>
+      <div className='background__red'></div>
+      <div className='background__yellow'></div>
+      <div className='background__blue'></div>
+      <div className='background__green'></div>
+    </main>
+  );
+}
+
+export default Table;
