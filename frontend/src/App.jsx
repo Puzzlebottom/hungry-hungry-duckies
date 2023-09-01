c79606069c87a8636bb0d08f7f52278d8151cf28
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-01 16:27:36 -0400

    Add useState, add 2 views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,50 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { socket } from './socket';
+import { ConnectionState } from './components/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager';
+
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [munch, setMunch] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [home, setHome] = useState(false);
+  const [join, setJoin] = useState(false); //????????
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('home', () => setHome(true))
+    socket.on('join', (data) => console.log(data)) // ????
+    socket.on('munch', (event) => console.log(event)) // Get timestamp from event then setMunch(true)
+    socket.on('ready', (data) => setReady(true))
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('home', () => setHome(true))
+      socket.off('join', (data) => console.log(data))
+      socket.off('munch', (event) => console.log(event))
+      socket.off('ready', (data) => setReady(true))
+    };
+  }, []);
+
   return (
-    <>
-      <h1>Hello Duckies</h1>
-    </>
+    <div className="App">
+      <ConnectionState isConnected={isConnected} />
+      <ConnectionManager />
+      <MyForm />
+    </div>
   );
 }
 
