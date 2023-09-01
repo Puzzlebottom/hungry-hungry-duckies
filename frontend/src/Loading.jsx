5c25ec177fa25f24812e5438e32be1b123d5733c
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-01 14:38:25 -0400

    adds static markup for loading view

---
+++ frontend/src/Loading.jsx
@@ -0,0 +1,18 @@
+import './Loading.css';
+
+function Loading() {
+  return (
+    <main>
+      <div className="loading-duck">
+        <img src="http://placekitten.com/500/400"></img>
+      </div>
+      <h1 className='loading-message'>LOADING...</h1>
+      <div>
+        <h3>Spacebar to eat bugs</h3>
+        <h3>Spacebar to eat quack</h3>
+      </div>
+    </main>
+  );
+}
+
+export default Loading;
