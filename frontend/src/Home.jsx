49281a7b2e7d613a4fdd0b2db0836eaa132d2c5c
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-02 12:05:13 -0400

    Create seed file for database, create Leaderboard componenent and remove leaderboard home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <main>
       <div>
         <div className='logo'>
-          <img src="http://placekitten.com/500/400"></img>
+          <img src="https://i.redd.it/a7w8qv77ctl31.png"></img>
         </div>
         <section>
           <h3>Spacebar to eat bugs</h3>
@@ -13,21 +13,6 @@ function Home() {
         </section>
       </div >
       <div>
-        <article className='leaderboard'>
-          <header>Leaderboard</header>
-          <ul>
-            <li>1. Super Dude 25</li>
-            <li>2. Super Dude 25</li>
-            <li>3. Super Dude 25</li>
-            <li>4. Super Dude 25</li>
-            <li>5. Super Dude 25</li>
-            <li>6. Super Dude 25</li>
-            <li>7. Super Dude 25</li>
-            <li>8. Super Dude 25</li>
-            <li>9. Super Dude 25</li>
-            <li>10. Super Dude 25</li>
-          </ul>
-        </article>
         <form className='player-name-form'>
           <input type='text' name='playerName' placeholder='Enter Player Name'></input>
         </form>
---
+++ frontend/src/components/Leaderboard.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+export default function Leaderboard() {
+const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/players')
+      .then((response) => {
+        const sortedPlayers = response.data.sort((a, b) => b.hi_score - a.hi_score);
+        setPlayers(sortedPlayers);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="leaderboard">
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th><User></User>name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player, index) => (
+            <tr key={player.id}>
+              <td>{index + 1}</td>
+              <td>{player.name}</td>
+              <td>{player.hi_score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
