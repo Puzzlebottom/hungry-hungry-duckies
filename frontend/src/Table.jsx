9ca24a25609eabd659c4b41a94fa4b8867415f14
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-02 10:26:31 -0400

    adds static markup for table view

--- frontend/src/Table.jsx
+++ frontend/src/Table.jsx
@@ -8,14 +8,36 @@ import './Table.css';
 function Table() {
   return (
     <main>
-      <div className='background__green'></div>
-      <div className='background__red'></div>
-      <div className='background__blue'></div>
-      <div className='background__yellow'></div>
       <img src={arena} className='table'></img>
+      <span className='countdown'>GO!</span>
+      <span className='timer'>12:59</span>
+      <div className='background background__green'></div>
+      <div className='player-detail-panel player-detail-panel__top-left'>
+        <span className='player-name'>Player Name</span>
+        <span className='score'>0</span>
+        <button className='button__ready'>Ready?</button>
+      </div>
       <img src={duckieTopLeft} className='duckie__top-left'></img>
+      <div className='background background__red'></div>
+      <div className='player-detail-panel player-detail-panel__top-right'>
+        <span className='player-name'>Player Name</span>
+        <span className='score'>0</span>
+        <button className='button__ready'>Ready?</button>
+      </div>
       <img src={duckieTopRight} className='duckie__top-right'></img>
+      <div className='background background__blue'></div>
+      <div className='player-detail-panel player-detail-panel__bottom-left'>
+        <span className='player-name'>Player Name</span>
+        <span className='score'>0</span>
+        <button className='button__ready'>Ready?</button>
+      </div>
       <img src={duckieBottomLeft} className='duckie__bottom-left'></img>
+      <div className='background background__yellow'></div>
+      <div className='player-detail-panel player-detail-panel__bottom-right'>
+        <span className='player-name'>Player Name</span>
+        <span className='score'>0</span>
+        <button className='button__ready'>Ready?</button>
+      </div>
       <img src={duckieBottomRight} className='duckie__bottom-right'></img>
     </main>
   );
