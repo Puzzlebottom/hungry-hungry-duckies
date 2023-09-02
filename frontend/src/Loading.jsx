1c9634e2ffd3ca7e125a263cfe1abfa24acc91e7
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-02 12:54:04 -0400

    adds markup for loading view

--- frontend/src/Loading.jsx
+++ frontend/src/Loading.jsx
@@ -1,15 +1,24 @@
 import '../public/stylesheets/Loading.css';
+import duckieTopLeft from './assets/duckie-top-left-no-shadow-1.png';
+import duckieTopRight from './assets/duckie-top-right-no-shadow-1.png';
+import duckieBottomLeft from './assets/duckie-bottom-left-no-shadow-1.png';
+import duckieBottomRight from './assets/duckie-bottom-right-no-shadow-1.png';
 
 function Loading() {
   return (
     <main>
-      <div className="loading-duck">
-        <img src="http://placekitten.com/500/400"></img>
+      <div className="duckie-container rotate">
+        <img src={duckieTopLeft} className='duckie__top-left'></img>
+        <img src={duckieTopRight} className='duckie__top-right'></img>
+        <img src={duckieBottomLeft} className='duckie__bottom-left'></img>
+        <img src={duckieBottomRight} className='duckie__bottom-right'></img>
       </div>
-      <h1 className='loading-message'>LOADING...</h1>
-      <div>
-        <h3>Spacebar to eat bugs</h3>
-        <h3>Spacebar to eat quack</h3>
+      <span className='loading-message'>
+        <h3>LOADING...</h3>
+      </span>
+      <div className='instructions'>
+        <span>Spacebar to eat bugs</span>
+        <span>Spacebar to quack</span>
       </div>
     </main>
   );
