46effde4a721e39281e8bdbf860094846f45006f
Your GitHub Username <Your GitHub Email>
2023-09-02 14:41:35 -0700

    mute/unmute working

--- frontend/src/components/PlaySound.jsx
+++ frontend/src/components/PlaySound.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Howl, Howler } from 'howler';
 
+
 const PlaySound = () => {
   const [isMuted, setIsMuted] = useState(true);
   const [audio, setAudio] = useState(null);
-
 
   useEffect(() => {
     const sound = new Howl({
@@ -16,7 +16,6 @@ const PlaySound = () => {
     });
 
     setAudio(sound);
-
 
     return () => {
       sound.stop();
@@ -32,13 +31,45 @@ const PlaySound = () => {
     <div>
       <button onClick={toggleMute}>
         {isMuted ? (
-          <span role="img" aria-label="Muted">
-            🔇
-          </span>
+          <div
+            style={{
+              backgroundColor: 'transparent',
+              borderRadius: '50%',
+              width: '30px',
+              height: '30px',
+              display: 'inline-block',
+            }}
+          >
+            <img
+              src="https://freesvg.org/img/pink_button_sound_on_morgaine1976.png"
+              alt="Muted"
+              style={{
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+              }}
+            />
+          </div>
         ) : (
-          <span role="img" aria-label="Unmuted">
-            🔊
-          </span>
+          <div
+            style={{
+              backgroundColor: 'transparent',
+              borderRadius: '50%',
+              width: '30px',
+              height: '30px',
+              display: 'inline-block',
+            }}
+          >
+            <img
+              src="https://freesvg.org/img/pink_button_sound_off_morgaine1976.png"
+              alt="Unmuted"
+              style={{
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+              }}
+            />
+          </div>
         )}
       </button>
     </div>
