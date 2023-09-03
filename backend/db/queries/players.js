9d81c6c54c3914d2238b90a406a1b9f615f14fbc
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-03 16:00:42 -0400

    Add cors middleware and axios. Create players api route that returns all players as json. Add useEffect and use state to home view that contains all players and their hi score

--- backend/db/queries/players.js
+++ backend/db/queries/players.js
@@ -1,1 +1,10 @@
 const db = require('../connection');
+
+const getAllPlayers = () => {
+  const query = `
+    SELECT * FROM players;
+  `;
+  return db.query(query);
+}
+
+exports.getAllPlayers = getAllPlayers;
--- backend/db/schema/seed.sql
+++ backend/db/schema/seed.sql
@@ -1,38 +1,38 @@
--- Insert a player with a unique UUID and different data
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Epic Gamer', 60, 'd37e39af-2f8a-4aa1-9ec0-8ea740addd68', 4, 20, 4, '2023-09-02 12:00:00');
 
--- Insert another player with distinct information
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Gaming Guru', 90, '7e98d49b-35e2-45f6-b8e5-63cde9e5d2db', 5, 40, 5, '2023-09-02 18:20:00');
 
--- Insert a player with a different name and high score
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Masterful Player', 85, 'b413a049-4829-4db5-9c17-ef7c10d019a0', 6, 70, 6, '2023-09-02 22:45:00');
 
--- Insert a player with a unique UUID and game ID
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Skillful Gamer', 70, 'c782f18e-ae2b-4bb0-b650-9a88cb634297', 7, 15, 7, '2023-09-02 14:10:00');
 
--- Insert a player with a different name and current seat
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Top Scorer', 120, 'f52d45a6-9d74-48d9-b30b-d487a40f7a77', 8, 80, 8, '2023-09-02 17:30:00');
 
--- Insert a player with a unique UUID and higher current score
+
 INSERT INTO players (name, hi_score, cookie_uuid, game_id, current_score, current_seat, created_at) VALUES ('Ultimate Gamer', 110, 'a9f0c6f4-2f8d-4f4f-85bf-0577f1982b26', 9, 95, 9, '2023-09-02 21:00:00');
 
 
 
 
--- Insert a new game with an active status
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('192.168.1.100', true, '2023-09-02 08:30:00');
 
--- Insert another game with a different socket address
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('10.0.0.50', false, '2023-09-02 10:15:00');
 
--- Insert a game with an active status and a unique socket address
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('172.16.0.75', true, '2023-09-02 12:45:00');
 
--- Insert a game with an inactive status and a specific timestamp
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('192.168.0.10', false, '2023-09-02 15:20:00');
 
--- Insert a game with an active status and a different socket address
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('10.1.1.200', true, '2023-09-02 17:00:00');
 
--- Insert a new game with an inactive status and the current timestamp
+
 INSERT INTO games (socket_address, is_active, created_at) VALUES ('192.168.2.30', false, NOW());
