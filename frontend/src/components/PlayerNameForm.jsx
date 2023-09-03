4b521bbde7c6cbfc38fd2ddd0da5883e7d67aea9
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-03 11:55:15 -0400

    Change player name form into controlled component with useState

--- frontend/src/components/PlayerNameForm.jsx
+++ frontend/src/components/PlayerNameForm.jsx
@@ -1,10 +1,13 @@
-
+import { useState } from 'react'
 
 export default function PlayerNameForm () {
+  const [playerName, setPlayerName] = useState('')
+
   return (
     <form className='playername-form'>
       <label htmlFor='name'>Name</label>
-      <input type='text' name='playerName' placeholder='Enter Player Name'/>
+      <input type='text' id='name' name='name' value={playerName} onChange={(event) => setPlayerName(event.target.value)}
+      placeholder='Enter player name' />
       <button type='submit'>Submit</button>
     </form>
   )
