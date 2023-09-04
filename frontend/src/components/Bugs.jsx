2de1747ac2324c223604b75cd6a65233ebdec948
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-04 15:09:35 -0400

    adds render cleanup to fix timing issues

--- frontend/src/components/Bugs.jsx
+++ frontend/src/components/Bugs.jsx
@@ -1,6 +1,6 @@
 import Matter from "matter-js";
 import MatterAttractors from "matter-attractors";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import bug1 from '../assets/bug1.png';
 import bug2 from '../assets/bug2.png';
@@ -8,104 +8,116 @@ import bug2 from '../assets/bug2.png';
 Matter.use(MatterAttractors);
 const { Engine, Render, Runner, Body, Bodies, Composite, Events, Vector } = Matter;
 
+const TOTAL_BOUNDARY_FACES = 100; // controls the smoothness of the arena walls; more faces = smoother walls
+const WALL_SEGMENT_DIMENSIONAL_COEFFICIENT = 0.15; // controls the thickness of the arena walls; bigger number = thicker walls
+const INSIDE_DIAMETER_ADJUSTMENT = 0.436; // used to scale boundary radius to match arena.png; bigger number = bigger arena
+const ATTRACTION_COEFFICIENT = 132e-11; // contols how strongly bugs are pulled toward the center. default 5e-7
+const NUMBER_OF_BUGS = 25;
+const BUG_SIZE_COEFFECIENT = 44e-3; // scales the bug physics object; bigger number = bigger bugs
+const SPRITE_SIZE_COEFFECIENT = 71e-5; // scales the bug sprite; bigger number = bigger sprite
+const SPRITE_Y_OFFSET = -0.05; // shifts the sprite to more accurately match the apparent center of gravity
+const BUG_FRICTION_COEFFICIENT = -57e-5; // controls the negative friction of the bugs applying an innate churn without input; default -57e-5
+const AIR_FRICTION_COEFFICIENT = 41e-6; // controls how rapidly the bugs slow down; bigger number = slower bugs
+const RESTITUTION = 1; // controls the bounciness of the bugs; bigger number = more bouncy;
+
 export default function Bugs(props) {
-  const arena = useRef();
+  const arena = useRef(); // arena element
   const engine = useRef(Engine.create());
-  let tickCounter = useRef(0);
+  let tickCounter = useRef(0); // counts engine render updates
+
+  const [viewWidth, setViewWidth] = useState(document.body.clientWidth);
+  const [viewHeight, setViewHeight] = useState(document.body.clientHeight);
+
 
   const handleResize = (e) => {
-    console.log(e);
+    setViewWidth(document.body.clientWidth);
+    setViewHeight(document.body.clientHeight);
   };
 
   useEffect(() => {
 
     window.addEventListener('resize', handleResize);
 
-    const width = document.body.clientWidth;
-    const height = document.body.clientHeight;
+    // const viewWidth = document.body.clientWidth;
+    // const viewHeight = document.body.clientHeight;
 
     const render = Render.create({
       element: arena.current,
       engine: engine.current,
       options: {
-        width: width,
-        height: height,
+        width: viewWidth,
+        height: viewHeight,
         wireframes: false,
         background: 'transparent',
       }
     });
 
-    render.engine.gravity.scale = 0;
+    render.engine.gravity.scale = 0; // removes standard y-axis gravity; we're looking down at a table
 
-    const centerpoint = { x: width / 2, y: height / 2 };
-    const numberOfSides = 100;
-    const smallerSide = Math.min(width, height);
-    let ratio = 0.45;
-    let radius = smallerSide * ratio;
-    console.log(radius * 2);
+    const centerpoint = { x: viewWidth / 2, y: viewHeight / 2 };
+    const smallerSide = Math.min(viewWidth, viewHeight);
+    const radius = smallerSide * INSIDE_DIAMETER_ADJUSTMENT;
 
-    const getCenter = () => {
+    const getAttractor = () => {
+      const attractor = (bodyA, bodyB) => {
+        const a = bodyA.position;
+        const b = bodyB.position;
+        return { x: (a.x - b.x) * radius * ATTRACTION_COEFFICIENT, y: (a.y - b.y) * radius * ATTRACTION_COEFFICIENT * radius / 350 };
+      };
+
       const center = Bodies.circle(centerpoint.x, centerpoint.y, 0, {
         isStatic: true,
-        plugin: {
-          attractors: [
-            function(bodyA, bodyB) {
-              return {
-                x: (bodyA.position.x - bodyB.position.x) * 1e-6,
-                y: (bodyA.position.y - bodyB.position.y) * 1e-6,
-              };
-            }
-          ]
-        }
+        plugin: { attractors: [attractor] }
       });
-      Body.setMass(center, 1);
 
       return center;
     };
 
-
     const getBounds = () => {
-      const segments = [];
+      const wallSegments = [];
+      const totalSegments = TOTAL_BOUNDARY_FACES;
+      const segmentSideLength = WALL_SEGMENT_DIMENSIONAL_COEFFICIENT * smallerSide;
 
-      for (let i = 0; i < numberOfSides; i++) {
-        let totalSegments = numberOfSides;
-        let angle2 = i / numberOfSides * Math.PI * 2 + totalSegments / 2;
-        let x2 = Math.cos(angle2);
-        let y2 = Math.sin(angle2);
-        let cx2 = x2 * radius + width / 2;
-        let cy2 = y2 * radius + height / 2;
-        let rect = Bodies.rectangle(cx2, cy2, 45 / 1000 * smallerSide, 150 / 1000 * smallerSide, { angle: angle2, isStatic: true, render: { visible: false } });
-        segments.push(rect);
+      for (let i = 0; i < totalSegments; i++) {
+        const segmentAngle = i / totalSegments * Math.PI * 2 + totalSegments / 2;
+        const segmentCenter = {
+          x: Math.cos(segmentAngle) * (radius + segmentSideLength / 2) + centerpoint.x,
+          y: Math.sin(segmentAngle) * (radius + segmentSideLength / 2) + centerpoint.y
+        };
+        const segment = Bodies.rectangle(segmentCenter.x, segmentCenter.y, segmentSideLength, segmentSideLength,
+          { angle: segmentAngle, isStatic: true, render: { visible: false } }
+        );
+        wallSegments.push(segment);
 
       }
-      return segments;
+      return wallSegments;
     };
 
-    function getRandomCoordinateInCircle(radius) {
+    const getRandomCoordinateInCircle = (radius) => {
       const x = Math.random() * radius * Math.cos(Math.random() * 2 * Math.PI);
       const y = Math.random() * radius * Math.sin(Math.random() * 2 * Math.PI);
       return { x, y };
-    }
+    };
 
     const getBugs = () => {
       const bugs = [];
-      console.log('RADIUS: ', radius * 0.044);
-      for (let i = 0; i < 25; i++) {
-        const position = getRandomCoordinateInCircle(radius / Math.PI);
-        const newBug = Bodies.circle(width / 2 + position.x, height / 2 + position.y, radius * 0.044, {
-          restitution: 1, friction: -0.2 * (radius / 350), frictionAir: 0.01, frictionStatic: 0, label: 'bug',
+
+      for (let i = 0; i < NUMBER_OF_BUGS; i++) {
+        const randomOffset = getRandomCoordinateInCircle(radius / Math.PI);
+        const x = centerpoint.x + randomOffset.x;
+        const y = centerpoint.y + randomOffset.y;
+        const size = radius * BUG_SIZE_COEFFECIENT;
+        const newBug = Bodies.circle(x, y, size, {
+          restitution: RESTITUTION, friction: radius * BUG_FRICTION_COEFFICIENT, frictionAir: radius * AIR_FRICTION_COEFFICIENT, frictionStatic: 0, label: 'bug',
           render: {
             sprite: {
               texture: bug1,
-              xScale: 0.25 * radius / 350,
-              yScale: 0.25 * radius / 350,
-              // the offset is to more accurately line up the bugs center of mass with the sprite
-              xOffset: 0,
-              yOffset: -0.05
+              xScale: radius * SPRITE_SIZE_COEFFECIENT,
+              yScale: radius * SPRITE_SIZE_COEFFECIENT,
+              yOffset: SPRITE_Y_OFFSET
             }
           }
         });
-        Body.setMass(newBug, 0.5 * radius / 350);
         bugs.push(newBug);
       }
       return bugs;
@@ -125,18 +137,12 @@ export default function Bugs(props) {
     const alignBug = (bug) => {
       const velocity = Body.getVelocity(bug);
       const bearing = Vector.angle({ x: 0, y: 0 }, velocity);
-      // - Math.:PI / 2 is needed because the png is facing the wrong way.
-      Body.setAngle(bug, bearing - Math.PI / 2);
+      Body.setAngle(bug, bearing - Math.PI / 2); // Math.PI / 2 (-90deg) adjustment is needed because the png is facing the wrong way.
     };
 
-    const center = getCenter();
-    const bounds = getBounds();
-    const bugs = getBugs();
-
-    const composite = Composite.add(engine.current.world, [center, ...bounds, ...bugs]);
+    const composite = Composite.add(engine.current.world, [getAttractor(), ...getBounds(), ...getBugs()]);
 
     Events.on(engine.current, 'afterUpdate', () => {
-
       const bugs = Matter.Composite.allBodies(composite).filter(body => body.label === 'bug');
       tickCounter.current++;
 
@@ -146,13 +152,12 @@ export default function Bugs(props) {
         }
         alignBug(bug);
       }
-
       if (tickCounter.current === 20) tickCounter.current = 0;
     });
 
     Render.run(render);
 
-    let runner = Runner.create();
+    const runner = Runner.create();
     Runner.run(runner, engine.current);
 
     return () => {
