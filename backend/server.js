533bb7855ff45f4d5f2e538530f30af581699931
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-04 15:50:22 -0400

    Add updatePlayerName and addPlayer queries. Use those in server.js when playerName emission occurs

--- backend/server.js
+++ backend/server.js
@@ -64,32 +64,6 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 
-  // socket.on('checkPlayerCookie', (cookie) => {
-  //   console.log('SERVER CHECK COOKIE: ', cookie);
-  //   if (cookie.cookie_uuid) {
-  //   const query = `
-  //     SELECT * FROM players
-  //     WHERE cookie_uuid = $1;
-  //   `;
-  //   const values = [cookie.cookie_uuid];
-  //   db.query(query, values)
-  //     .then(data => {
-  //       if(data.rows[0]) {
-  //         console.log('cookie_uuid exists');
-  //         socket.emit('checkCookieReply', { 'msg': `server says: name => ${data.rows[0].name}`, 'name': data.rows[0].name });
-  //       } else {
-  //         console.log('cookie_uuid does not exist');
-  //         socket.emit('checkCookieReply', { 'msg': `server says: name => ''`, 'name': null });
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.log('err: ', err);
-  //     });
-  //   } else {
-  //     socket.emit('checkCookieReply', { 'msg': `server says: no cookie found`, 'name': null });
-  //   }
-  // });
-
   socket.on('playerName', (playerNameObj) => {
     console.log('playerName: ' + playerNameObj.name);
     console.log('cookie_uuid: ' + playerNameObj.cookie_uuid);
@@ -98,14 +72,9 @@ io.on('connection', (socket) => {
       .then(data => {
         if(data.rows[0]) {
           console.log('player exists');
-          if(data.rows[0].name !== playerNameObj.name) {
+          if(data.rows[0].name !== name) {
             console.log('name changed');
-            db.query(`
-            UPDATE players
-            SET name = $1
-            WHERE cookie_uuid = $2
-            RETURNING *;
-            `, [playerNameObj.name, playerNameObj.cookie_uuid])
+            playerQueries.updatePlayerName(cookie_uuid, name)
               .then(data => {
                 socket.emit('serverReply', {'msg': `server says: name => ${data.rows[0].name}`, 'name': data.rows[0].name});
               })
@@ -117,11 +86,7 @@ io.on('connection', (socket) => {
           }
         } else {
           console.log('player does not exist');
-          db.query(`
-          INSERT INTO players (cookie_uuid, name)
-          VALUES ($1, $2)
-          RETURNING *;
-          `, [playerNameObj.cookie_uuid, playerNameObj.name])
+          playerQueries.addPlayer(cookie_uuid, name)
             .then(data => {
               socket.emit('serverReply', {'msg': `server says: name => ${data.rows[0].name}`, 'name': data.rows[0].name});
             })
