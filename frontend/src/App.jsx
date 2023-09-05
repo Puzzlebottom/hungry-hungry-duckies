dfc29cfe476ea32fffd206efc2742a1b61bd4e69
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-05 16:40:43 -0400

    Merge feature/centralized-state-logic into master

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Home from './Home';
 import Table from './Table';
 import PostGame from './PostGame';
@@ -15,23 +16,10 @@ const {
 
 const { defaultName, view, leaderBoardPlayers } = state;
 
-  const [backgroundMusic, setBackgroundMusic] = useState(true);
-
-  useEffect(() => {
-    setBackgroundMusic(view === 'home');
-  }, [view]);
-
-  //Emits player name form info to server
-  const handleSubmission = (name) => {
-    socket.emit('join', { 'name': name, 'cookie_uuid': cookies.cookie_uuid });
-  };
-  const handleViewChange = (view) => {
-    setView(view);
-  };
   const currentView = () => {
     switch (view) {
       case 'home':
-        return <Home { ...{ handleSubmission, defaultName, handleViewChange } } />;
+        return <Home { ...{ handleSubmission, defaultName, handleViewChange, leaderBoardPlayers } } />;
       case 'table':
         return <Table { ...{ handleViewChange } } />;
       case 'postGame':
@@ -39,42 +27,16 @@ const { defaultName, view, leaderBoardPlayers } = state;
       case 'loading':
         return <Loading  { ...{ handleViewChange } } />;
       default:
-        return <Home { ...{ handleSubmission, defaultName, handleViewChange } } />;
+        return <Home { ...{ handleSubmission, defaultName, handleViewChange, leaderBoardPlayers } } />;
     }
   };
 
-  useEffect(() => {
-    socket.on('joinReply', (response) => setCookie('name', response.name, { path: '/' }));
-    socket.on("connect_error", (err) => {
-      console.log(`connect_error due to ${err.message}`);
-    });
-
-    return () => {
-      socket.off('joinReply', (response) => setCookie('name', response.name, { path: '/' }));
-      socket.off("connect_error", (err) => {
-        console.log(`connect_error due to ${err.message}`);
-      });
-    };
-  }, []);
+  // background music
+  const [backgroundMusic, setBackgroundMusic] = useState(true);
 
   useEffect(() => {
-    setBackgroundMusic(view === 'home');
-  }, [view]);
-
-  useEffect(() => {
-    axios.get('http://localhost:8080/', { withCredentials: true })
-      .then((response) => {
-        console.log('RESPONSE FROM AXIOS', response);
-        const { name, cookie_uuid } = response.data;
-        setCookie('cookie_uuid', cookie_uuid, { path: '/' });
-        setCookie('name', name, { path: '/' });
-        setDefaultName(name);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
+    setBackgroundMusic(state.view === 'home');
+  }, [state.view]);
 
   return (
     <div className="App">
