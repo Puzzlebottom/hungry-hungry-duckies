6c3eb456e5437a760ac271b1e61aa2be4a8da99f
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-09 11:24:23 -0400

    updates frontend imports

--- frontend/src/hooks/useBugs.js
+++ frontend/src/hooks/useBugs.js
@@ -1,10 +1,11 @@
 import Matter from "matter-js";
 import MatterAttractors from "matter-attractors";
+import BugConstants from "../constants/bugConstants";
 import { useEffect, useRef, useState } from 'react';
 
 import bug1 from '../assets/bug1.png';
 import bug2 from '../assets/bug2.png';
-import { TOTAL_BOUNDARY_FACES, WALL_SEGMENT_DIMENSIONAL_COEFFICIENT, INSIDE_DIAMETER_ADJUSTMENT, ATTRACTION_COEFFICIENT, BUG_SIZE_COEFFICIENT, BUG_FRICTION_COEFFICIENT, BUG_TEMPO, SPRITE_SIZE_COEFFECIENT, SPRITE_Y_OFFSET, AIR_FRICTION_COEFFICIENT, RESTITUTION } from '../constants/bugConstants';
+const { TOTAL_BOUNDARY_FACES, WALL_SEGMENT_DIMENSIONAL_COEFFICIENT, INSIDE_DIAMETER_ADJUSTMENT, ATTRACTION_COEFFICIENT, BUG_SIZE_COEFFICIENT, BUG_FRICTION_COEFFICIENT, BUG_TEMPO, SPRITE_SIZE_COEFFECIENT, SPRITE_Y_OFFSET, AIR_FRICTION_COEFFICIENT, RESTITUTION } = BugConstants;
 
 Matter.use(MatterAttractors);
 const { Engine, Render, Runner, Body, Bodies, Composite, Events, Vector } = Matter;
