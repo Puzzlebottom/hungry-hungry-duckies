2bb659bef093dc4400197f9e4352785000e484af
Your GitHub Username <Your GitHub Email>
2023-09-09 12:40:26 -0700

    squashed some bugs

--- frontend/src/PostGame.jsx
+++ frontend/src/PostGame.jsx
@@ -13,40 +13,45 @@ function PostGame({ gameState, home, newGame }) {
   }, []);
 
   return (
-    <div className="Leaderboard-container">
-
-      <h1 className="Winner">Leaderboard</h1>
-      <table className="true-leaderboard-table">
-        <thead>
-          <tr className="player-column-categories">
-            <th className="player-name-header">Player Name</th>
-            <th className="player-score-header">Score</th>
-            <th className="player-status-header">Status</th>
-          </tr>
-        </thead>
-        <tbody className="post-game-playas">
-          {players.current.map((player, index) => (
-            <tr className={index === 0 ? 'custom-first-row' : 'player-column'} key={index}>
-              <td className="player-name">{player.name}</td>
-              <td className="player-score">{player.current_score}</td>
-              <td className={`player-status ${index === 0 ? 'winner' : 'loser'}`}>
-                {index === 0 ? 'Winner' : 'Loser'}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="container-for-post-game-buttons">
-        <button className="main-menu-button1" onClick={() => home()}>Main Menu</button>
-        <button className="play-again-button" onClick={() => newGame(name.current)}>Play again</button>
-      </div>
-      <div className="audiobutton">
-
+    <div>
+      <div className="background-container space" />
+      <div className="background-container planets" />
+      <div className="background-container meteors" />
+      <div className="background-container stars" />
+      <div className="Leaderboard-container-post">
+        <h4 className="Winner">Leaderboard</h4>
+        <div className="leaderboard-container-menu">
+          <table className="true-leaderboard-table">
+            <thead className="header-container-post">
+              <tr>
+                <th className="player-name-header">Player Name</th>
+                <th className="player-score-header">Score</th>
+                <th className="player-status-header">Status</th>
+              </tr>
+            </thead>
+            <tbody className="post-game-playas">
+              {players.current.map((player, index) => (
+                <tr className={index === 0 ? 'custom-first-row' : 'player-column'} key={index}>
+                  <td className="player-name">{player.name}</td>
+                  <td className="player-score">{player.current_score}</td>
+                  <td className={`player-status ${index === 0 ? 'winner' : 'loser'}`}>
+                    {index === 0 ? 'Winner' : 'Loser'}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="container-for-post-game-buttons">
+            <button className="main-menu-button1" onClick={() => home()}>Main Menu</button>
+            <button className="play-again-button" onClick={() => newGame(name.current)}>Play again</button>
+          </div>
+          <div className="audiobutton" />
+        </div>
       </div>
     </div>
   );
-
 }
+
 
 
 export default PostGame;
