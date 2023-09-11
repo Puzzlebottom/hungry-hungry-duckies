48861978b68acb7724d08e64fd24585428a279ce
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-11 10:10:10 -0400

    makes player detail panel responsive

--- frontend/src/components/PlayerDetailPanel.jsx
+++ frontend/src/components/PlayerDetailPanel.jsx
@@ -4,10 +4,10 @@ export default function PlayerDetailPanel({ name, current_seat, current_score, i
   const seats = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
 
   return (
-    <div className={'player-detail-panel player-detail-panel__' + seats[current_seat]}>
-      <span className='player-name'>{name}</span>
-      <span className='score'>{current_score}</span>
-      {!isActive && <button className='button__ready' onClick={toggleReady}>{isReady ? 'Ready!' : 'Ready?'}</button>}
-    </div>
+    <section className={`player-detail-panel ${seats[current_seat]}`}>
+      <span>{name}</span>
+      <span>{current_score}</span>
+      {!isActive && <button className={isReady ? 'ready' : ''} onClick={toggleReady}>{isReady ? 'Ready!' : 'Ready?'}</button>}
+    </section>
   );
 }
