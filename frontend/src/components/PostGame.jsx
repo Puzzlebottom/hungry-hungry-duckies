71f237126bbf55fc184cc83574e2fb8e8df73cce
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-09 17:08:48 -0400

    cleanup

--- frontend/src/components/PostGame.jsx
+++ frontend/src/components/PostGame.jsx
@@ -36,41 +36,6 @@ function PostGame({ gameState, home, newGame }) {
           <button onClick={() => newGame(name.current)}>Play again</button>
         </nav>
       </section>
-
-      {/* <div className="background-container space" style={{ backgroundImage: `url(${backgrounds.PostGame[0]})` }}></div>
-      <div className="background-container planets" style={{ backgroundImage: `url(${backgrounds.PostGame[1]})` }}></div>
-      <div className="background-container meteors" style={{ backgroundImage: `url(${backgrounds.PostGame[2]})` }}></div>
-      <div className="background-container stars" style={{ backgroundImage: `url(${backgrounds.PostGame[3]})` }}></div>
-      <div className="Leaderboard-container-post">
-        <h4 className="Winner">Leaderboard</h4>
-        <div className="leaderboard-container-menu">
-          <table className="true-leaderboard-table">
-            <thead className="header-container-post">
-              <tr>
-                <th className="player-name-header">Player Name</th>
-                <th className="player-score-header">Bugs Munched!</th>
-                <th className="player-status-header">Status</th>
-              </tr>
-            </thead>
-            <tbody className="post-game-playas">
-              {players.current.map((player, index) => (
-                <tr className={index === 0 ? 'custom-first-row' : 'player-column'} key={index}>
-                  <td className="player-name">{player.name}</td>
-                  <td className="player-score">{player.current_score}</td>
-                  <td className={`player-status ${index === 0 ? 'winner' : 'loser'}`}>
-                    {index === 0 ? 'Winner' : 'Loser'}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="container-for-post-game-buttons">
-            <button className="main-menu-button1" onClick={() => home()}>Main Menu</button>
-            <button className="play-again-button" onClick={() => newGame(name.current)}>Play again</button>
-          </div>
-          <div className="audiobutton" />
-        </div>
-      </div> */}
     </main>
   );
 }
